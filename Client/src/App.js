f40ada71d9896cbbc960ba344323843b2d9092f0
Greg Plumblee <gplumblee@hotmail.com>
2020-04-01 11:45:27 -0700

    initial push of meeting list page with components

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from "react";
-import { BrowserRoute as Router, Route} from "react-router-dom"
-import Contact from "./Pages/ContactPage"
-import Inspire from "./Pages/InspirationPage"
-import Landing from "./Pages/LandingPage"
-import MeetingAdd from "./Pages/MeetingCreate"
-import Meeting from "./Pages/MeetingsPage"
-import Other from "./Pages/otherGroups"
-import Profile from "./Pages/profilePage"
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './components/Nav';
+import Landing from  './pages/Landing';
+import Contact from "./pages/ContactPage"
+import Inspire from "./pages/InspirationPage"
+import Landing from "./pages/LandingPage"
+import Meetings from './pages/Meetings';
+import MeetingAdd from "./pages/MeetingCreate"
+import Other from "./pages/otherGroups"
+import Profile from "./pages/profilePage"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Route exact path="/" component={Landing} />
+      <main className="container">
+        <Route exact path="/" component={()=><Landing username='someone'/>} />
         <Route exact path="/contactpage" component={Contact} />
         <Route exact path="/inspire" component={Inspire} />
-        <Route exact path="/meetingreation" component={MeetingAdd} />
-        <Route exact path="/meetingpage" component={Meeting} />
+        <Route exact path="/meetingcreation" component={MeetingAdd} />
+        <Route exact path='/meetings' component={()=><Meetings username='someone'/>}/>
         <Route exact path="/othergroups" component={Other} />
         <Route exact path="/profilepage" component={Profile} />
-      </div>
+      </main>
     </Router>
   );
 }
-
 
 export default App;
