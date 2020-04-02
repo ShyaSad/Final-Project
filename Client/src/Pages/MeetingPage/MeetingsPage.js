aeb01c447ec5c7762fb5cead217bea732d22b859
Greg Plumblee <gplumblee@hotmail.com>
2020-04-01 22:11:08 -0700

    fixed pathways so meetings page works with master setup

--- Client/src/Pages/MeetingPage/MeetingsPage.js
+++ Client/src/Pages/MeetingPage/MeetingsPage.js
@@ -1,10 +1,96 @@
-// our contact info
-import React from "react";
+// when the meetings are via api SQL or Mongo
 
-const MeetingsPage = () => {
-    return (
-        <h1>This is MeetingsPage Page</h1>
-    )
-}
+import React, { useState } from "react";
+import seed from "../../utils/meetingSeed";
+import MeetingCard from "../../components/MeetingCards";
+import Dropdown from '../../components/Dropdown';
 
-export default MeetingsPage;
+const date = new Date();
+const hour = date.getHours();
+const day = date.getUTCDay();
+console.log(day, hour);
+
+export default () => {
+  const [page, setPage] = useState(1);
+  const [meetings, setMeetings] = useState(seed);
+  const [filteredMeetings, setFilteredMeetings] = useState(seed);
+
+  const handleFilter =(arg)=>{
+      setFilteredMeetings(meetings.filter(a=>a.day === arg))
+  }
+
+  return (
+    <>
+      <Dropdown handleFilter={handleFilter}/>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item" onClick={()=>setPage(page-1)}>
+            <a className="page-link" href="#">
+              Previous
+            </a>
+          </li>
+          <li className="page-item" onClick={()=>setPage(1)}>
+            <a className="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" onClick={()=>setPage(2)} href="#">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" onClick={()=>setPage(3)} href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" onClick={()=>setPage(page+1)} href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {filteredMeetings
+        .sort((a, b) =>
+          a.time.split(":")[0] < b.time.split(":")[0]
+            ? 1
+            : a.time.split(":")[0] > b.time.split(":")[0]
+            ? -1
+            : 0
+        )
+        .map(item => (
+          <MeetingCard key={item.code} meeting={item} />
+        ))}
+      <nav aria-label="Page navigation example">
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Previous
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
