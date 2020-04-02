808574090b4e4c4475c5aef93f37a0bcd044ed42
Kevin Reyes <kev.andrew.reyes@gmail.com>
2020-04-01 21:51:56 -0700

    initial push

--- Client/src/Pages/OtherGroups/OtherGroups.js
+++ Client/src/Pages/OtherGroups/OtherGroups.js
@@ -1,10 +1,79 @@
-// our contact info
-import React from "react";
+import React from 'react';
 
 const OtherGroups = () => {
     return (
-        <h1>This is Contact Page</h1>
-    )
-}
+
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                </tr>
+            </thead>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>SF and Marin County AA Online Meetings</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Northern California AA Virtual Meeting List</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Washington DC AA Online Meetings</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Online International AA Meeting Directory</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>London AA Online Meetings</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Scotland AA Online Meetings</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Paris AA Online Meetings in English</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>AA Intergroup Online Meetings</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>AA Grapevine - All 2020 issues free online!</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>In The Rooms (Online Recovery Meetings)</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Lion Rock Online AA & Support Groups</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Facebook Recovery Meetings (must request to join)</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>LA Meetings on Facebook</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>AA London - 7 Hours Ahead of PST</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Tel Aviv Virtual AA - 9 Hours Ahead of PST</td>
+                </tr>
+                <th scope="row">3</th>
+                <td>Jerusalem Virtual AA - 9 Hours Ahead of PST</td>
+    </tr>
+</table >
+);
+};
 
 export default OtherGroups;
