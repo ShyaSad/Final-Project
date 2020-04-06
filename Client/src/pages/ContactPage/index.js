7a66146c0b3070f710f9cd718c9d82210cf640aa
Greg Plumblee <gplumblee@hotmail.com>
2020-04-06 10:40:00 -0700

    straightened up my local drive

--- Client/src/pages/ContactPage/index.js
+++ Client/src/pages/ContactPage/index.js
@@ -1,34 +1,11 @@
+// our contact info
 import React from "react";
-// import Example from "../../components/Navbar";
-import {Container} from 'reactstrap';
-import {Jumbotron} from 'reactstrap';
-import ContactCard from "../../components/ContactCard";
-import './style.css';
 import "./Contact.css";
 
-// import "./OtherGroups.css"
-const ContactGroups = () => {
-  const contacts = [{ "name": "  Greg Plumblee", "title": "Front-End Developer", "url": "https://github.com/gplumblee", "email": "gplumblee@hotmail.com"  },
-  { "name": "Jose Antonio Morales", "title": "Front-End Developer", "url": "https://github.com/VisualDesign3r", "email": "visualdesign3r@gmail.com" },
-  { "name": "Shya Sadeghi-Tabrizi ", "title": "Back-End Developer", "url": "https://github.com/ShyaSad", "email": "shyasadeghi@gmail.com"},
-  { "name": "Kevin Reyes", "title": "Front-End Developer", "url": "https://github.com/kevin-reyes", "email": "kev.andrew.reyes@gmail.com"},]
-  const contactResourceCards = contacts.map(contact => <ContactCard name= {contact.name} title={contact.title} url={contact.url} email={contact.email}/>)  ;
-  return (
-    <React.Fragment>
-      <Jumbotron className="jumbotron">
-      <Container>
-      <h1 className="text-center" >Contact</h1>
-        {contactResourceCards}
-
-          {/* <div className="position">
-           
-          </div> */}
-          
-       
-      </Container>
-      </Jumbotron>
-    </React.Fragment>
-  )
+const ContactPage = () => {
+    return (
+        <h1>This is Contact Page</h1>
+    )
 }
-export default ContactGroups;
 
+export default ContactPage;
--- Client/src/pages/OtherGroups/index.js
+++ Client/src/pages/OtherGroups/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ResourceCards from "../../components/ResourceCards";
+import {Container} from "reactstrap";
 import "./OtherGroups.css";
 
 // import "./OtherGroups.css"
