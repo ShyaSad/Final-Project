fd563efa2a283c7bb0ebff17dc72806efdf3889b
shyasadeghi@gmail.com <shyasadeghi@gmail.com>
2020-04-06 16:37:33 -0700

    added axios functions

--- Client/src/utils/API.js
+++ Client/src/utils/API.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 export default {
 
-    getMeetings: ()=> axios.get('/meetings/all'),
+    getMeetings: ()=> axios.get('/meetings/allmeetings'),
+    createMeeting: () => axios.post('/meeting/create', params),
+    getDailyReflections: () => axios.get('/daily/inspirations'),
     login: (params)=> axios.post('/auth/login', params),
     signUp: (params)=> axios.post('/auth/signup', params)
+
 }
