2e5012e5508c6708dfa2584a0748601cc909a923
shyasadeghi@gmail.com <shyasadeghi@gmail.com>
2020-04-06 17:20:34 -0700

    added code for axios calls

--- Client/src/pages/LoginPage/index.js
+++ Client/src/pages/LoginPage/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
 import "./LoginPage.css";
+import login from "../../utils/API";
+
+const LoginFunc = () => {
+  const data = {
+    email: "email",
+    password: "test"
+  };
+  login.login(data).then(res => console.log(res)).catch(err => console.log(err))
+}
 
 const Example = (props) => {
   return (
@@ -15,7 +24,7 @@ const Example = (props) => {
         <Label for="examplePassword">Password</Label>
         <Input type="password" name="password" id="examplePassword" placeholder="Password" />
       </FormGroup>
-      <Button>Submit</Button>
+      <Button onClick={LoginFunc}>Submit</Button>
     </Form>
     </Container>
   );
